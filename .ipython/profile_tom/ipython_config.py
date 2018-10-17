137f0b090ed6bbeb6dc3746764ffb0db1c97b844
Thomas Cooper <tom.n.cooper@gmail.com>
2018-10-17 09:42:23 +0100

    Added caplock alias

--- .ipython/profile_tom/ipython_config.py
+++ .ipython/profile_tom/ipython_config.py
@@ -29,7 +29,9 @@
 #c.InteractiveShellApp.exec_files = []
 
 ## lines of code to run at IPython startup.
-#c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
 
 ## A list of dotted module names of IPython extensions to load.
 #c.InteractiveShellApp.extensions = []
