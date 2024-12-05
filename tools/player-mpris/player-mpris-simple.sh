49d2285bfd5e7f0c44708d0ec328d10e9f16f3f9
Thomas Cooper <code@tomcooper.dev>
2024-12-05 15:35:57 +0000

    Add character limit to player-mpris script output

--- tools/player-mpris/player-mpris-simple.sh
+++ tools/player-mpris/player-mpris-simple.sh
@@ -3,9 +3,9 @@
 player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "Playing: $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo "Playing: $(playerctl metadata artist) - $(playerctl metadata title)" | cut -c -50
 elif [ "$player_status" = "Paused" ]; then
-    echo "Paused: $(playerctl metadata artist) - $(playerctl metadata title)"
+    echo "Paused: $(playerctl metadata artist) - $(playerctl metadata title)" | cut -c -50
 else
     echo "No Media"
 fi
